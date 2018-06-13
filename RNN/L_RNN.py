4749a388abafa74748da19d6ead49e5778aaa31c
Yi Tian Xu <yxu219@cim.mcgill.ca>
2018-06-13 11:39:36 -0400

    compare

--- RNN/L_RNN.py
+++ RNN/L_RNN.py
@@ -151,7 +151,7 @@ class L_LSTM:
 		# metrics.plot_metrics(self, data_iterator, valid_data)
 		# association_evaluation.eval_generation(self, valid_data, data_iterator)
 		# association_evaluation.eval_center(self, valid_data, 'sitting')
-		association_evaluation.transfer_motion(self, valid_data, 'walking','greeting')
+		association_evaluation.transfer_motion(self, valid_data, 'sitting', 'greeting')
 		# association_evaluation.eval_generation_from_label(self, data_iterator)
 		# metrics.plot_metrics_labels(self, data_iterator, valid_data)
 		# metric_baselines.compare_label_embedding(self, data_iterator)
