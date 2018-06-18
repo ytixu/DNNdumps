9d7dd87fb416d3eaa8d610c7f6fe21a3751498bb
yitian <yi.t.xu@mail.mcgill.ca>
2018-06-18 15:38:16 -0400

    fix

--- Forward/utils/parser.py
+++ Forward/utils/parser.py
@@ -10,15 +10,14 @@ def get_model_load_name(model_name):
 def get_log_name(model_name):
 	return '../models/%s_%d.log'%(model_name, time.time())
 
-def get_data():
-	data = np.load('../data/z_L_RNN.npy')
+def get_data(data_path='../data/z_L_RNN.npy'):
+	data = np.load()
 	return data[:,1], data[:,2]
 
 def get_parse(model_name, labels=False):
 	ap = argparse.ArgumentParser()
 	list_of_modes = ['train', 'sample']
-	ap.add_argument('-id', '--input_data', required=True, help='Input data directory')
-	ap.add_argument('-od', '--output_data', required=True, help='Output data directory')
+	ap.add_argument('-id', '--data_path', required=True, help='Input data directory')
 	ap.add_argument('-m', '--mode', required=False, help='Choose between training mode or sampling mode.', default='train', choices=list_of_modes)
 	ap.add_argument('-ep', '--epochs', required=False, help='Number of epochs', default='5', type=int)
 	ap.add_argument('-bs', '--batch_size', required=False, help='Batch size', default='16', type=int)
@@ -26,6 +25,6 @@ def get_parse(model_name, labels=False):
 	ap.add_argument('-sp', '--save_path', required=False, help='Model save path', default=get_model_load_name(model_name))
 	ap.add_argument('-p', '--periods', required=False, help='Number of iterations of the data', default='10', type=int)
 
-	x, y = get_data(args['input_data'], args['output_data'])
+	x, y = get_data(args['data_path'])
 
 	return x, y, args
