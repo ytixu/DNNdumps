2ef96b7798670b12a74fa7c34f12dc41079d2a70
Yi Tian Xu <yxu219@cim.mcgill.ca>
2018-06-24 10:48:13 -0400

    d

--- RNN/L_RNN.py
+++ RNN/L_RNN.py
@@ -12,7 +12,7 @@ import keras.backend as K
 from utils import parser, image, embedding_plotter, recorder, metrics, metric_baselines, association_evaluation
 from Forward import NN
 
-LEARNING_RATE = 0.0005
+LEARNING_RATE = 0.00001
 NAME = 'L_LSTM'
 USE_GRU = True
 if USE_GRU:
@@ -39,7 +39,7 @@ class L_LSTM:
 		print self.labels
 		self.input_dim = args['input_dim'] + self.label_dim
 		self.output_dim = args['output_dim'] + self.label_dim
-		self.hierarchies = args['hierarchies'] if 'hierarchies' in args else [14, 19, 29]
+		self.hierarchies = args['hierarchies'] if 'hierarchies' in args else [24,49,69]
 		self.latent_dim = args['latent_dim'] if 'latent_dim' in args else (args['input_dim']+args['output_dim'])/2
 		self.trained = args['mode'] == 'sample' if 'mode' in args else False
 		self.load_path = args['load_path']
@@ -172,9 +172,9 @@ class L_LSTM:
 		# iter1, iter2 = tee(data_iterator)
 		# metrics.validate(valid_data, self)
 
-		nn = NN.Forward_NN({'input_dim':self.latent_dim, 'output_dim':self.latent_dim, 'mode':'sample'})
-		nn.run(None)
-		# metrics.plot_metrics(self, data_iterator, valid_data, nn)
+		#nn = NN.Forward_NN({'input_dim':self.latent_dim, 'output_dim':self.latent_dim, 'mode':'sample'})
+		#nn.run(None)
+		metrics.plot_metrics(self, data_iterator, valid_data, None)
 		# association_evaluation.eval_generation(self, valid_data, data_iterator)
 		# association_evaluation.eval_center(self, valid_data, 'sitting')
 		# association_evaluation.transfer_motion(self, valid_data, 'sitting', 'walking', data_iterator)
@@ -183,7 +183,7 @@ class L_LSTM:
 		# association_evaluation.eval_generation_from_label(self, data_iterator)
 		# association_evaluation.plot_add(self, data_iterator)
 		# metrics.plot_metrics_labels(self, data_iterator, valid_data)
-		metric_baselines.compare_label_embedding(self, nn, data_iterator)
+		#metric_baselines.compare_label_embedding(self, nn, data_iterator)
 		# association_evaluation.eval_distance(self, valid_data)
 		# evaluate.eval_pattern_reconstruction(self.encoder, self.decoder, iter2)
 
