67eef52a9ddb609616445fee0091b3b1cf6db8d9
Yi Tian Xu <yxu219@cim.mcgill.ca>
2018-07-11 11:14:09 -0400

    t

--- human_motion_pred/src/utils/data_utils__.py
+++ human_motion_pred/src/utils/data_utils__.py
@@ -223,6 +223,7 @@ def readCSVasFloat_randLines(filename, timesteps, rand_n, one_hot, action_n):
     rand_n = max(int(math.floor(line_n-16-timesteps)),rand_n)
     # Sample somewherein the middle (from seq2seq_model.get_batch)
     line_idx = [16+i for i in range(rand_n)] # np.random.choice(line_n-timesteps-16, rand_n, replace=False)+16
+    print (line_idx)
     data_dim = lines[0].shape[-1]
     if one_hot:
       returnArray = np.zeros((rand_n, timesteps, data_dim+action_n))
